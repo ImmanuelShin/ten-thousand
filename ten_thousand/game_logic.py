e7bd74d8d8873e699245213010a5ed0f54ec4af1
Immanuel Shin <Immanuel.shin18@gmail.com>
2024-01-16 13:49:00 -0800

    Accidental save

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -1,9 +1,28 @@
 import random
 
 class GameLogic:
+  """
+  A class to represent the game logic for a dice game, such as Dice 10000.
+
+  This class provides methods for rolling dice and calculating scores based 
+  on the outcome of the dice rolls.
+  """
 
   @staticmethod
   def calculate_score(dice):
+    """
+    Calculate the score for a given roll of dice in Dice 10000.
+
+    The score is calculated based on a set of rules which include straights,
+    three pairs, sets of a kind (like three or more 1s), and leftover 1s and 5s.
+
+    Parameters:
+    dice (tuple): A tuple of integers representing the outcome of the dice roll.
+
+    Returns:
+    int: The calculated score for the given roll of dice.
+    """
+
     score = 0
     rolls = [dice.count(i) for i in range(1,7)]
 
@@ -39,4 +58,16 @@ class GameLogic:
   
   @staticmethod
   def roll_dice(dice):
+    """
+    Calculate the score for a given roll of dice in Dice 10000.
+
+    The score is calculated based on a set of rules which include straights,
+    three pairs, sets of a kind (like three or more 1s), and leftover 1s and 5s.
+
+    Parameters:
+    dice (tuple): A tuple of integers representing the outcome of the dice roll.
+
+    Returns:
+    int: The calculated score for the given roll of dice.
+    """
     return tuple(random.randint(1, 6) for _ in range(0,dice))
